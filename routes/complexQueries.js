322951f7fc2b7614f46f271fdb6bf8d7d1815ac0
David Arhin <davidarhin2005@gmail.com>
2024-05-02 04:47:45 +0000

    regex and other complex queries

--- routes/complexQueries.js
+++ routes/complexQueries.js
@@ -1,21 +1,36 @@
+const { Double } = require("mongodb");
 const express = require('express');
 const ComplexQueries = require('../models/complexQueries');
 const { getDb } = require('../db');
-
 const complex_router = express.Router();
 
-complex_router.get('/complex', async (req, res) => {
-    const product_id = req.params.product_id;
-    const price = parseFloat(req.query.price);
-    
+
+
+complex_router.get('/transaction/:amount', async (req, res) => {
+    const amount = parseFloat(req.params.amount);
+
     try {
-        const products = await ComplexQueries.findProductsByCategoryAndPriceRange(product_id, price);
+        // console.log("first")
+        const transactions = await ComplexQueries.findTransactionsWithTotalAmountGreaterThan(amount);
+        res.json(transactions);
+        // console.log("first")
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Error retrieving transactions" });
+    }
+});
+
+complex_router.get('/product/:keyword', async (req, res) => {
+    const keyword = req.params.keyword;
+
+    try {
+        const products = await ComplexQueries.findProductsByNameKeyword(keyword);
         res.json(products);
-        console.log("first")
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: "Error retrieving products" });
     }
 });
+
 
 module.exports = complex_router;
